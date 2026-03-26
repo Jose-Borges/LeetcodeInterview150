601f0913e1a2ae5b71b31cd0ece574d6add5a780
jborges <jsborges.239@gmail.com>
2026-03-26 11:25:22 +0000

    Testing again
    Signed-off-by: jborges <jsborges.239@gmail.com>

--- src/ArrayString/RemoveElement.java
+++ src/ArrayString/RemoveElement.java
@@ -2,15 +2,15 @@ package ArrayString;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        int k = 0;
+        int j = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        return k;
+        return j;
     }
 }
