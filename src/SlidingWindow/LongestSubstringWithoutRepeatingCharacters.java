cf4c8ca20d285188ac4f4a22475a4e794774e9d2
jborges <jsborges.239@gmail.com>
2026-03-30 12:06:59 +0100

    LongestSubstringWithoutRepeatingCharacters.java7 completed
    Signed-off-by: jborges <jsborges.239@gmail.com>

---
+++ src/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,22 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                if (map.get(c) >= left) {
+                    left = map.get(c) + 1;
+                }
+            }
+            map.put(c, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
