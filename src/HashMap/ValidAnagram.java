77770b8454e1eec50120d8acdebeb0d3b5f9d808
jborges <jsborges.239@gmail.com>
2026-04-16 10:58:52 +0100

    ValidAnagram.java implemented
    Signed-off-by: jborges <jsborges.239@gmail.com>

---
+++ src/HashMap/ValidAnagram.java
@@ -0,0 +1,34 @@
+package HashMap;
+
+import TwoPointers.ValidPalindrome;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            map.put(currChar, map.getOrDefault(currChar, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char currChar = t.charAt(i);
+            int currCount = map.getOrDefault(currChar, -1);
+            if (currCount <= 0) {
+                return false;
+            }
+            map.put(currChar, currCount - 1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram validAnagram = new ValidAnagram();
+        validAnagram.isAnagram("rat", "car");
+    }
+}
