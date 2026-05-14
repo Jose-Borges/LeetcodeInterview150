eb87cf0199e84fb88bdc2341cf082f6578816274
José Borges <jsborges.239@gmail.com>
2026-05-14 14:14:08 +0100

    GasStation implemented.

---
+++ src/ArrayString/GasStation.java
@@ -0,0 +1,27 @@
+package ArrayString;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0, totalCost = 0;
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        if (totalGas < totalCost) {
+            return -1;
+        }
+
+        int currentGas = 0;
+        int startingPoint = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            currentGas += gas[i] - cost[i];
+            if (currentGas < 0) {
+                currentGas = 0;
+                startingPoint = i + 1;
+            }
+        }
+        return startingPoint;
+    }
+}
